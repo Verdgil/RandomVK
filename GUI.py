b0fbdb8b78e3da5d3fd927697c969f514f6516ed
Verdgil <me@verdgil.org>
2016-07-02 22:20:53 +0300

    Version 1.0

---
+++ GUI.py
@@ -0,0 +1,206 @@
+import backend
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, \
+    QLineEdit, QGridLayout, QMessageBox
+from PyQt5.QtGui import QIcon, QPixmap
+import sys
+import json
+import os
+import vlc
+import tempfile
+import threading
+import time
+
+class Login(QWidget):
+    login = ''
+    passwd = ''
+    grid = QGridLayout()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('RandomVK')
+        self.setWindowIcon(QIcon('web.png'))
+        self.Login_Label = QLabel("Login:")
+        self.Login_Input = QLineEdit('')
+        self.Passwd_Label = QLabel("Passwd:")
+        self.Passwd_Input = QLineEdit('')
+        self.Passwd_Input.setEchoMode(QLineEdit.Password)
+        self.Login_Button = QPushButton('Login')
+        self.Login_Button.clicked.connect(self.onClick_login)
+        self.grid.setSpacing(9)
+        self.grid.addWidget(self.Login_Label, 0, 0)
+        self.grid.addWidget(self.Login_Input, 0, 1)
+        self.grid.addWidget(self.Passwd_Label, 1, 0)
+        self.grid.addWidget(self.Passwd_Input, 1, 1)
+        self.grid.addWidget(self.Login_Button, 1, 2)
+        self.setLayout(self.grid)
+        self.show()
+    def onClick_login(self):
+        try:
+            login = self.Login_Input.text()
+            passwd = self.Passwd_Input.text()
+            global vk
+            vk = backend.vk_audio(login=login, passwd=passwd)
+            #raise backend.vk.exceptions.VkAuthError
+            pass
+        except backend.vk.exceptions.VkAuthError as VkAuthError:
+            text = VkAuthError.args[0]
+            err = QMessageBox()
+            err.setText(text)
+            err.setDefaultButton(QMessageBox.Ok)
+            err.setText(text)
+            err.buttonClicked.connect(self.msgbtn)
+            retval = err.exec_()
+        else:
+            if os.name == "nt":
+                Setting_File = open(os.getcwd() + '\\setting.json', 'w')
+            else:
+                Setting_File = open(os.getcwd() + '/setting.json', 'w')
+            acc = {'access_token': vk.get_access_token()}
+            Setting_Text = json.dumps(acc)
+            print(Setting_Text, file=Setting_File, end='')
+            Setting_File.close()
+            self.hide()
+            Player_UI.show()
+            Player_UI.Next_Button.click()
+            self.close()
+
+class Player(QWidget):
+    grid = QGridLayout()
+    def __init__(self):
+        super().__init__()
+        self.instance = vlc.Instance()
+        self.mediaplayer = self.instance.media_player_new()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('RandomVK')
+        self.setWindowIcon(QIcon('web.png'))
+        self.Author_Label = QLabel()
+        self.Name_Label = QLabel()
+        self.Add_Button = QPushButton('Add')
+        self.Add_Button.clicked.connect(self.Click_Add)
+        self.PP_Button = QPushButton('Play')
+        self.PP_Button.clicked.connect(self.Click_PP)
+        self.Next_Button = QPushButton('Next')
+        self.Next_Button.clicked.connect(self.Click_Next)
+        self.Song_Image_Label = QLabel(self)
+        self.Song_Image = QPixmap('blank.png')
+        self.Song_Image_Label.setPixmap(self.Song_Image)
+        self.Cost_Label = QLabel('-')
+        self.grid.setSpacing(9)
+        self.grid.addWidget(self.Song_Image_Label, 0, 1)
+        self.grid.addWidget(self.Author_Label, 1, 0)
+        self.grid.addWidget(self.Cost_Label, 1, 1)
+        self.grid.addWidget(self.Name_Label, 1, 2)
+        self.grid.addWidget(self.Add_Button, 2, 0)
+        self.grid.addWidget(self.Next_Button, 2, 2)
+        self.grid.addWidget(self.PP_Button, 2, 1)
+        self.setLayout(self.grid)
+
+    def Click_Next(self):
+        self.audio = vk.get_audio()
+        temp = tempfile.gettempdir()
+        if os.name == 'nt':
+            temp += '\\RandomVK'
+        else:
+            temp += '/RandomVK'
+        if not os.path.exists(temp):
+            os.mkdir(temp)
+        resp = backend.requests.get(self.audio['url'])
+        if 'image' in self.audio:
+            img = backend.requests.get(self.audio['image'])
+        else:
+            self.Song_Image.load('blank.png')
+        if os.name == 'nt':
+            tempfile_rand = open(temp + '\\temp.mp3', 'wb')
+            tempfile_rand.write(resp.content)
+            tempfile_rand.close()
+            self.media = self.instance.media_new(temp + '\\temp.mp3')
+            if 'image' in self.audio:
+                tmp_image = open(temp + '\\temp.png', 'wb')
+                tmp_image.write(img.content)
+                tmp_image.close()
+                self.Song_Image.load(temp + '\\temp.png')
+        else:
+            tempfile_rand = open(temp + '/temp.mp3', 'wb')
+            tempfile_rand.write(resp.content)
+            tempfile_rand.close()
+            self.media = self.instance.media_new(temp + '/temp.mp3')
+            if 'image' in self.audio:
+                tmp_image = open(temp + '/temp.png', 'wb')
+                tmp_image.write(img.content)
+                tmp_image.close()
+                self.Song_Image.load(temp + '/temp.png')
+        try:
+            self.mediaplayer.stop()
+        except:
+            pass
+        self.Song_Image_Label.setPixmap(self.Song_Image)
+        self.mediaplayer.set_media(self.media)
+        self.mediaplayer.play()
+        self.time_last = time.time()
+        self.PP_Button.setText('Pause')
+        self.Author_Label.setText(self.audio['artist'])
+        self.Name_Label.setText(self.audio['title'])
+        self.thread = threading.Thread(target=self.th)
+        self.thread.daemon = True
+        self.thread.start()
+    def Click_Add(self):
+        global vk
+        vk.add(self.audio['id'], self.audio['aid'])
+    def Click_PP(self):
+        if self.mediaplayer.is_playing():
+            self.mediaplayer.pause()
+            self.PP_Button.setText("Play")
+            self.time_last = time.time()
+            del self.thread
+        else:
+            self.mediaplayer.play()
+            self.time_last -= time.time()
+            self.audio['duration'] -= int(self.time_last)
+            self.audio['duration'] *= -1
+            self.audio['duration'] += 15
+            self.PP_Button.setText('Pause')
+            self.thread = threading.Thread(target=self.th)
+            self.thread.daemon = True
+            self.thread.start()
+
+    def th(self):
+        time.sleep(int(self.audio['duration']))
+        self.Next_Button.click()
+
+
+
+
+if __name__ == '__main__':
+    vk = 0
+    app = QApplication(sys.argv)
+    Player_UI = Player()
+    try:
+        Setting_file = open("setting.json", 'r')
+    except FileNotFoundError:
+        Login_UI = Login()
+    else:
+        Setting_json = ''
+        Setting_dict = Setting_file.readlines()
+        Setting_file.close()
+        for s in Setting_dict:
+            Setting_json += s
+        if '{' in Setting_json:
+            setting = json.loads(Setting_json)
+            if 'access_token' in setting:
+                try:
+                    vk = backend.vk_audio(acc=setting['access_token'])
+                except vk.exceptions.VkAuthError as VkAuthError:
+                    Login_UI = Login()
+                else:
+                    Player_UI.show()
+                    Player_UI.Next_Button.click()
+            else:
+                Login_UI = Login()
+        else:
+            Login_UI = Login()
+        print()
+    sys.exit(app.exec_())
---
+++ backend.py
@@ -0,0 +1,82 @@
+import json
+import requests
+import vk
+from random import randint
+
+class vk_audio:
+    app_id = 3020234
+    api = 0
+    now_play_audio_id = {'id': 0, 'aid': 0}
+    access_token = ''
+    def __init__(self, login = '', passwd = '', acc = ''):
+        self.app_id = 3020234
+        self.now_play_audio_id = {'id': 0, 'aid': 0}
+        try:
+            if (login != '' and passwd != ''):
+                session = vk.AuthSession(app_id=str(self.app_id),
+                                         user_login=login, user_password=passwd,
+                                         scope='audio, offline')
+            elif acc != '':
+                session = vk.Session(access_token=acc)
+            else:
+                raise vk.exceptions.VkAuthError
+        except vk.exceptions.VkAuthError as VkAuthError:
+            raise VkAuthError
+        self.access_token = session.access_token
+        self.api = vk.API(session)
+    def get_audio(self):
+        user_id = randint(1, 5000000)
+        do = False
+        while not do:
+            try:
+                audio_user = self.api.audio.get(user_id=user_id)
+            except vk.exceptions.VkAPIError as err:
+                user_id = randint(1, 5000000)
+            else:
+                if len(audio_user) > 0:
+                    do = True
+                else:
+                    user_id = randint(1, 5000000)
+        lastfm = last()
+        audio_num = randint(0, len(audio_user) - 1)
+        self.now_play_audio_id['id'] = user_id
+        self.now_play_audio_id['aid'] = audio_user[audio_num]['aid']
+        artist = audio_user[audio_num]['artist']
+        title = audio_user[audio_num]['title']
+        image = lastfm.get_image(artist, title)
+        if image == lastfm.Not_Found:
+            audio = {'url': audio_user[audio_num]['url'],
+                     'artist': artist,
+                     'title': title,
+                     'id': user_id,
+                     'aid': audio_user[audio_num]['aid'],
+                     'duration': audio_user[audio_num]['duration']}
+            return audio
+        audio = {'url': audio_user[audio_num]['url'],
+                 'artist': artist,
+                 'title': title,
+                 'duration': audio_user[audio_num]['duration'],
+                 'id': user_id,
+                 'aid': audio_user[audio_num]['aid'],
+                 'image': image}
+        return audio
+    def add(self, id, aid):
+        self.api.audio.add(owner_id=id, audio_id=aid)
+    def get_access_token(self):
+        return self.access_token
+
+class last:
+    api_key = 'ebbf28d2f2b3f66122332e8ad63a321b'
+    Not_Found = 'Not found'
+    def __init__(self):
+        pass
+    def get_image(self, artist, title):
+        # pylast.Track
+        url = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=' \
+              + 'ebbf28d2f2b3f66122332e8ad63a321b' + '&artist=' + artist +'&track=' + title + '&format=json'
+        anw = json.loads(requests.get(url).text)
+        if ('error' in anw) or ('album' not in anw['track']):
+            return self.Not_Found
+        image = anw['track']['album']['image']
+        image = image[len(image) - 1]['#text']
+        return image
